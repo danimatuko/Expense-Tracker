081bd5029bd4c0a8862a79da59b94b8aebaaf339
Dani Matuko <danimatuko@elementary61.lan>
2023-02-05 19:47:39 +0200

    add portfolio link to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,6 @@
-<footer class="fixed-bottom text-center py-3">
-    Copyright 2023 danimatuko
+<footer class="fixed-bottom text-center py-3 text-secondary">
+    Copyright 2023
+    <a href="https://www.dani-matuko.com/" class="text-decoration-none fw-semibold" style="color:palevioletred;" target="_blank">dani-matuko.com</a>
 </footer>
 </body>
 
